f77be7cefcae8eef42eb78ab5ca3a1dbcb84a87c
lightumcc <jessiezhu@createchstudio.com>
2025-09-19 11:33:05 -0400

    chore: docker build

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,18 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-  transpilePackages: ['three'],
-  reactStrictMode: true,
-  webpack(config, { dev }) {
-    if (dev) {
-      config.plugins = config.plugins.filter((plugin: object) => {
-        return true;
-        // return plugin.constructor.name !== 'ReactFreshWebpackPlugin';
-      });
-    }
-    return config;
-  },
+    transpilePackages: ['three'],
+    reactStrictMode: true,
+    webpack(config, {dev}) {
+        if (dev) {
+            config.plugins = config.plugins.filter((plugin: object) => {
+                return true;
+                // return plugin.constructor.name !== 'ReactFreshWebpackPlugin';
+            });
+        }
+        return config;
+    },
+    output: "standalone"
 };
 
 export default nextConfig;

471ad76b0e4af3c5448cd10f94f223f77b0b9876
Iewnfod <shangzhiqiuyue@outlook.com>
2025-09-22 17:10:34 +0800

    feat: use horizontal logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,7 +1,7 @@
 export default function Logo() {
     return (
         <img
-            src="/logo.png"
+            src="/logo-h.jpg"
             alt="No Safe Way Out"
             loading="eager"
             style={{height: '100%', userSelect: 'none'}}
